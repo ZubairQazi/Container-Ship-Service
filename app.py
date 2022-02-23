cb55e4bfa4758441bd7a0a49b448eae0e4fa7dfa
Zubair Qazi <zubair.qazi.5b@gmail.com>
2022-02-22 17:52:23 -0800

    Implement simple login page and dashboard

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "secretStuff" 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Route for handling the login page logic
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        session['user'] = username
+        return redirect('/dashboard')
+
+    return render_template("login.html")
+
+
+@app.route('/dashboard')
+def dashboard():
+    if('user' in session):
+        return render_template("dashboard.html", username=session['user'])
+
+    return '<h1>You are not logged in.</h1>'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/login')
