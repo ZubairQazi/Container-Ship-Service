3e9586f0919df84153f85eeacf24ed4e912a4d53
Saul Gonzalez <saul@sauls-air.lan>
2022-03-06 19:52:49 -0800

    Redesign dashboard and upload file verification

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect, session
+import os
+from flask import Flask, abort, render_template, request, redirect, session
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "secretStuff" 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.secret_key = "secretStuff"
+app.config['UPLOAD_EXTENSIONS'] = ['.txt']
+app.config['UPLOAD_PATH'] = 'uploads' 
 
 @app.route("/")
 def home():
@@ -25,15 +30,23 @@ def dashboard():
         return render_template("dashboard.html", username=session['user'])
 
     return '<h1>You are not logged in.</h1>'
+	
+@app.route('/service', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      if f != '':
+          file_ext = os.path.splitext(filename)[1]
+          if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+              return "Invalid file", 400
+          f.save(os.path.join(app.config['UPLOAD_PATH'],filename))
+      option = request.form['services']
+      if option == 'Transfer':
+          return render_template('transferService.html')
+      else:
+          return render_template('balanceService.html')
 
-
-@app.route('/transferService')
-def transfer():
-    return render_template('transferService.html')
-
-@app.route('/balanceService')
-def balance():
-    return render_template('balanceService.html')
 
 @app.route('/logout')
 def logout():
