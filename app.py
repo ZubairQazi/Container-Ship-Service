dcfbc8d9204b5decd394be6f7233eed1e5164742
Saul Gonzalez <saul@sauls-air.lan>
2022-03-10 02:13:49 -0800

    Balance Service almost complete and Transfer WIP

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from crypt import methods
+from dis import dis
 import os
+import re
+from shutil import move
+from sre_constants import SUCCESS
+
+from numpy import char
 import utils
-from flask import Flask, abort, render_template, request, redirect, session
+from flask import Flask, abort, render_template, request, redirect, session, url_for
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -27,7 +34,6 @@ def login():
 @app.route('/dashboard', methods = ['POST', 'GET'])
 def dashboard():
     if 'user' in session:
-
         return render_template("dashboard.html", username=session['user'])
 
     return '<h1>You are not logged in.</h1>'
@@ -43,6 +49,7 @@ def upload_file():
               return "Invalid file", 400
           filePath = os.path.join(app.config['UPLOAD_PATH'],filename)
           f.save(filePath)
+          session['filePath'] = filePath
           openFile = open(filePath,'r')
           containers = []
           ship_grid = utils.create_ship_grid(8,12)
@@ -50,9 +57,74 @@ def upload_file():
           ship_grid_flipped = ship_grid[::-1][:]
       option = request.form['services']
       if option == 'Transfer':
-          return render_template('transferService.html',ship_grid=ship_grid_flipped, enumerate=enumerate)
+          return render_template('initialTransferService.html',ship_grid=ship_grid_flipped, enumerate=enumerate,len=len)
       else:
-          return render_template('balanceService.html',ship_grid=ship_grid_flipped, enumerate=enumerate)
+          return render_template('initialBalancePage.html',ship_grid=ship_grid_flipped, enumerate=enumerate,len=len)
+
+#Transfer functions and html templates
+
+@app.route('/starttransfer', methods = ['GET','POST'])
+def start_transfer():
+    if request.method == 'POST':
+        filePath = session.get('filePath', None)
+        openFile = open(filePath,'r')
+        containers = []
+        ship_grid = utils.create_ship_grid(8,12)
+        openFile = open(filePath,'r')
+        utils.update_ship_grid(openFile,ship_grid,containers)
+        ship_grid_flipped = ship_grid[::-1][:]
+    return render_template('transferService.html',ship_grid=ship_grid_flipped, enumerate=enumerate,len=len,display_list=display_list,path_list=path_list,next_move_list=next_move_list)
+
+@app.route('/transfersteps', methods = ['GET','POST'])
+def next_step_transfer():
+    return render_template('transferService.html')
+
+@app.route('/transfercomplete', methods = ['GET', 'POST'])
+def transfered():
+    return render_template('transfered.html')
+
+#Balance functions and html templates
+
+@app.route('/startbalance', methods = ['GET','POST'])
+def start_balance():
+    if request.method == 'POST':
+        filePath = session.get('filePath', None)
+        openFile = open(filePath,'r')
+        containers = []
+        ship_grid = utils.create_ship_grid(8,12)
+        openFile = open(filePath,'r')
+        utils.update_ship_grid(openFile,ship_grid,containers)
+        ship_grid_flipped = ship_grid[::-1][:]
+        move_list,success = utils.balance(ship_grid,containers)
+        if move_list is not  None:
+            display_list = []
+            path_list = []
+            for step in move_list[0]:
+                numbers = re.findall(r"[^\[\],\sa-z]",step)
+                path_numbers = [chr(ord('7')-ord(numbers[0])+ord('0')),numbers[1],chr(ord('7')-ord(numbers[2])+ord('0')),numbers[3]]
+                adjusted_numbers = [chr(ord(num)+1) for num in numbers]
+                path_step = "["+path_numbers[0]+", "+path_numbers[1]+"] to ["+path_numbers[2]+", "+path_numbers[3]+"]"
+                display_step = "["+adjusted_numbers[0]+", "+adjusted_numbers[1]+"] to ["+adjusted_numbers[2]+", "+adjusted_numbers[3]+"]"
+                display_list.append(display_step)
+                path_list.append(path_step)
+            print(path_list)
+            next_move_list = move_list[1:]
+            session['next_move_list'] = next_move_list
+            if next_move_list is not None:
+                return render_template('balanceService.html',ship_grid=ship_grid_flipped, enumerate=enumerate,len=len,display_list=display_list,path_list=path_list,next_move_list=next_move_list)
+            else:
+                return render_template('balanced.html')
+
+@app.route('/balancesteps', methods = ['GET' , 'POST'])
+def next_step_balance():
+    if request.method == 'POST':
+        move_list = session.get('next_move_list',None)
+        print("After",move_list)
+    return render_template('balanced.html')
+
+@app.route('/balanced', methods = ['GET','POST'])
+def balanced():
+    return render_template('balanced.html')
 
 @app.route('/logout')
 def logout():
